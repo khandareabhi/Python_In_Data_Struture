c098bb729cd79ca2e19d1d514821f95178a8ed30
github.com/khandareabhi <khandareabhishek0@gmail.com>
2026-09-20 01:47:42 +0530

    Delete the beginning Element in Linked List

---
+++ Linked_List/delete_at_first.py
@@ -0,0 +1,23 @@
+class Node:
+    
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+node1=Node(10)
+node2=Node(20)
+node3=Node(30)
+
+node1.next=node2
+node2.next=node3
+
+head=node1
+
+#delete Logic in this 
+head=head.next
+
+current=head
+
+while current is not None:
+    print(current.data)
+    current=current.next
